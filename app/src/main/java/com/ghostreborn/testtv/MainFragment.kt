74da2112a03000c6cacd87d03679460b43d9e6f1
TorqueReborn <torquereborn@gmail.com>
2025-05-17 07:40:58 +0530

    Theme Setup

--- app/src/main/java/com/ghostreborn/testtv/MainFragment.kt
+++ app/src/main/java/com/ghostreborn/testtv/MainFragment.kt
@@ -1,6 +1,19 @@
 package com.ghostreborn.testtv
 
+import android.os.Bundle
+import androidx.core.content.ContextCompat
 import androidx.leanback.app.BrowseSupportFragment
 
 class MainFragment: BrowseSupportFragment() {
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        title = "App Name"
+        headersState = HEADERS_ENABLED
+        isHeadersTransitionOnBackEnabled = true
+        brandColor = ContextCompat.getColor(requireActivity(), R.color.brand_color)
+
+    }
+
 }
